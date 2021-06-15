be20362a52d2b531429b379e3aaca335fac2eb17
Code-Sush <85926217+Code-Sush@users.noreply.github.com>
2021-06-15 12:47:07 +0530

    Add files via upload

---
+++ Radius.py
@@ -0,0 +1,4 @@
+PI=3.14
+print("This Program Gets the input of the radius from the user and prints the area of the circle")
+numb=float(input("Input a radius: "))
+print("The area of the circle is", numb*numb*PI)
